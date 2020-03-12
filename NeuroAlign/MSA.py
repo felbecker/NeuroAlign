fixed bug with softmax output, modularized model, concat of multiple cores with unique param

NeuroAlign/MSA.py
@@ -87,27 +87,32 @@ class Instance:
         self.node_rp_targets = np.reshape(self.membership_targets / self.alignment_len, (-1,1))
 
 
-    #computes recall and precision of the edge predictions, if the
-    #correct number of columns was known
+    #computes recall and precision of the edge predictions
+    #over all possible pairs of positions in the sequences
+    # "true positive" = aligned in NR and reference
+    # "true negative" = not aligned in NR and reference
+    # "false positive" = aligned in NR but not in reference
+    # "false negative" = not aligned in NR but in reference
     def recall_prec(self, predictions):
 
         uncertainty_threshold = 0.2
-
+        print(predictions)
         tp, tn, fp, fn = 0,0,0,0
         choices = np.argmax(predictions, axis=1).flatten()
 
-        for i, (tar_n, choice) in enumerate(zip(self.membership_targets, choices)):
-            if tar_n == choice:
-                if predictions[i, choice] > uncertainty_threshold:
-                    tp += 1
+        for i, target_i in enumerate(self.membership_targets):
+            for j, choice_j in zip(range(i, len(self.membership_targets)), choices[i:]):
+                if target_i == choice_j:
+                    if predictions[j, choice_j] > uncertainty_threshold:
+                        tp += 1
+                    else:
+                        fn += 1
                 else:
-                    fn += 1
-            else:
-                if predictions[i, choice] > uncertainty_threshold:
-                    fp += 1
-                else:
-                    tn += 1
-
+                    if predictions[j, choice_j] > uncertainty_threshold:
+                        fp += 1
+                    else:
+                        tn += 1
+        print(tp, tn, fp, fn)
         prec = tp/(tp+fp) if tp+fp > 0 else 1
         rec = tp/(tp+fn) if tp+fn > 0 else 1
 

NeuroAlign/Model.py
@@ -64,17 +64,14 @@ def tile_like(graph, template_graphs):
                                       globals = tf.tile(graph.globals, multiples))
 
 
+#encodes the inputs of the neuroalign model
+#after encoding, one or more NeuroAlign Cores follow that them the
+#burden of optimization
+class NeuroAlignEncoder(snt.Module):
 
-#a module that computes the NeuroAlign prediction
-#output is a graph with 1D node and 1D edge attributes
-#nodes are sequence and pattern nodes with respective relative positions
-#edges correspond to region to pattern memberships with logits from which the
-#degrees of membership can be computed
-class SeqPatternGraphNet(snt.Module):
+    def __init__(self, config, name = "NeuroAlignEncoder"):
 
-    def __init__(self, config, name = "NeuroAlignPredictor"):
-
-        super(SeqPatternGraphNet, self).__init__(name=name)
+        super(NeuroAlignEncoder, self).__init__(name=name)
 
         self.seq_encoder = gn.modules.GraphIndependent(
                             edge_model_fn=make_mlp_model(config["seq_enc_edge_layer_s"]),
@@ -84,6 +81,23 @@ class SeqPatternGraphNet(snt.Module):
         self.mem_encoder = gn.modules.GraphIndependent(
                             node_model_fn=make_mlp_model(config["mem_enc_node_layer_s"]),
                             global_model_fn=make_mlp_model(config["mem_enc_globals_layer_s"]))
+
+    def __call__(self,
+                sequence_graph, #input graph with position nodes and forward edges describing the sequences
+                col_priors): #a graph tuple with a col prior graph for each column
+
+        latent_seq_graph = self.seq_encoder(sequence_graph)
+        latent_mem = self.mem_encoder(col_priors)
+        return latent_seq_graph, latent_mem
+
+
+#core module that computes the NeuroAlign prediction
+#output has to be passed to the NeuroAlignDecoder
+class NeuroAlignCore(snt.Module):
+
+    def __init__(self, config, name = "NeuroAlignCore"):
+
+        super(NeuroAlignCore, self).__init__(name=name)
 
         self.column_network = DeepSet(
                                     node_model_fn=make_mlp_model(config["column_net_node_layers"]),
@@ -96,36 +110,40 @@ class SeqPatternGraphNet(snt.Module):
                                     global_model_fn=make_mlp_model(config["seq_net_global_layers"]),
                                     reducer = tf.math.unsorted_segment_mean)
 
+    def __call__(self, latent_seq_graph, latent_mem, num_iterations):
+
+        latent_mem0 = latent_mem
+        tiled_latent_seq_graph0 = tile_like(latent_seq_graph, latent_mem)
+        for _ in range(num_iterations):
+            tiled_latent_seq = tile_like(latent_seq_graph, latent_mem)
+            col_input = gn.utils_tf.concat([tiled_latent_seq, latent_mem, latent_mem0], axis=1)
+            latent_mem = self.column_network(col_input)
+            seq_input = gn.utils_tf.concat([tiled_latent_seq_graph0, tiled_latent_seq, latent_mem], axis=1)
+            reduced = reduce_graphs(self.seq_network(seq_input))
+            latent_seq_graph = latent_seq_graph.replace(nodes = reduced.nodes, globals = reduced.globals)
+        return latent_seq_graph, latent_mem
+
+
+
+#decodes the output of a NeuroAlign core module
+class NeuroAlignDecoder(snt.Module):
+
+    def __init__(self, config, name = "NeuroAlignDecoder"):
+
+        super(NeuroAlignDecoder, self).__init__(name=name)
+
         self.seq_decoder = gn.modules.GraphIndependent(node_model_fn=make_mlp_model(config["seq_dec_node_layer_s"]))
 
         self.seq_output_transform = gn.modules.GraphIndependent(node_model_fn = lambda: snt.Linear(1, name="seq_output"))
 
         self.mem_decoder = gn.modules.GraphIndependent(node_model_fn=make_mlp_model(config["mem_dec_node_layer_s"]),
-                                                        global_model_fn=make_mlp_model(config["mem_dec_global_layer_s"]))
+                                global_model_fn=make_mlp_model(config["mem_dec_global_layer_s"]))
 
         self.mem_output_transform = gn.modules.GraphIndependent(node_model_fn = lambda: snt.Linear(1, name="mem_node_output"),
-                                                                global_model_fn = lambda: snt.Linear(1, name="mem_global_output"))
+                                        global_model_fn = lambda: snt.Linear(1, name="mem_global_output"))
 
 
-
-    def __call__(self,
-                sequence_graph, #input graph with position nodes and forward edges describing the sequences
-                seq_lens, #specifies the length of each input sequence, i.e. seq_lens[i] is the length of the ith chain in sequence_graph
-                col_priors, #a graph tuple with a col prior graph for each column
-                num_iterations): #the number of message passing iterations
-
-        latent_seq_graph = self.seq_encoder(sequence_graph)
-
-        latent_mem = self.mem_encoder(col_priors)
-        tiled_latent_seq_graph0 = tile_like(latent_seq_graph, latent_mem)
-
-        for _ in range(num_iterations):
-            tiled_latent_seq = tile_like(latent_seq_graph, latent_mem)
-            col_input = gn.utils_tf.concat([tiled_latent_seq, latent_mem], axis=1)
-            latent_mem = self.column_network(col_input)
-            seq_input = gn.utils_tf.concat([tiled_latent_seq_graph0, tiled_latent_seq, latent_mem], axis=1)
-            reduced = reduce_graphs(self.seq_network(seq_input))
-            latent_seq_graph = latent_seq_graph.replace(nodes = reduced.nodes, globals = reduced.globals)
+    def __call__(self, latent_seq_graph, seq_lens, latent_mem):
 
         #decode and convert to 1d outputs
         seq_out = self.seq_output_transform(self.seq_decoder(latent_seq_graph))
@@ -137,18 +155,42 @@ class SeqPatternGraphNet(snt.Module):
 
         #predict the distribution of the seq nodes to the columns using
         #a product of 2 softmaxes
-        mem_per_col = tf.reshape(mem_out.nodes, [sequence_graph.n_node[0], -1]) # (num_node, num_pattern)-tensor
+        mem_per_col = tf.reshape(mem_out.nodes, [latent_seq_graph.n_node[0], -1]) # (num_node, num_pattern)-tensor
         exp_mem_per_col = tf.exp(mem_per_col)
-        exp_mem_per_col_extended_a1 = tf.pad(exp_mem_per_col, [[0,0], [1,0]], constant_values=1) #extend with a column of ones for the uncertainty component of the softmax
-        sum_along_cols = tf.reduce_sum(exp_mem_per_col_extended_a1, axis=1, keepdims=True)
+        sum_along_cols = tf.reduce_sum(exp_mem_per_col, axis=1, keepdims=True)+1 #+1 to account for softmax uncertainty
         s_nodes = exp_mem_per_col/sum_along_cols
 
-        sum_along_seqs = tf.math.segment_sum(exp_mem_per_col, make_seq_ids(seq_lens))
-        s_cols = exp_mem_per_col/tf.reshape(tf.repeat(sum_along_seqs, seq_lens), [-1, 1])
+        sum_along_seqs = tf.math.segment_sum(exp_mem_per_col, make_seq_ids(seq_lens))+1 #-> (num_segments, num_pattern)-tensor
+        s_cols = exp_mem_per_col/tf.repeat(sum_along_seqs, repeats = seq_lens, axis = 0)
 
-        mem_final =  s_nodes * s_cols
+        return node_relative_positions, col_relative_positions, s_nodes, s_cols
 
-        return node_relative_positions, col_relative_positions, mem_final
+
+#a module that computes the NeuroAlign prediction
+#output is a graph with 1D node and 1D edge attributes
+#nodes are sequence and pattern nodes with respective relative positions
+#edges correspond to region to pattern memberships with logits from which the
+#degrees of membership can be computed
+class NeuroAlignModel(snt.Module):
+
+    def __init__(self, config, name = "NeuroAlignModel"):
+
+        super(NeuroAlignModel, self).__init__(name=name)
+        self.enc = NeuroAlignEncoder(config)
+        self.cores = [NeuroAlignCore(config) for _ in range(config["num_nr_core"])]
+        self.dec = NeuroAlignDecoder(config)
+
+
+    def __call__(self,
+                sequence_graph, #input graph with position nodes and forward edges describing the sequences
+                seq_lens, #specifies the length of each input sequence, i.e. seq_lens[i] is the length of the ith chain in sequence_graph
+                col_priors, #a graph tuple with a col prior graph for each column
+                num_iterations): #the number of message passing iterations
+
+        latent_seq_graph, latent_mem = self.enc(sequence_graph, col_priors)
+        for core in self.cores:
+            latent_seq_graph, latent_mem = core(latent_seq_graph, latent_mem, num_iterations)
+        return self.dec(latent_seq_graph, seq_lens, latent_mem)
 
 
 
@@ -158,7 +200,7 @@ class NeuroAlignPredictor():
 
     def __init__(self, config, examle_msa):
 
-        self.model = SeqPatternGraphNet(config)
+        self.model = NeuroAlignModel(config)
         self.checkpoint = tf.train.Checkpoint(module=self.model)
         self.checkpoint_root = "./checkpoints"
         self.checkpoint_name = "NeuroAlign"
@@ -181,9 +223,9 @@ class NeuroAlignPredictor():
 
     #col_priors is a list of position pairs (s,i) = sequence s at index i
     def predict(self, msa, num_cols):
-        seq_g, pattern_g = self._graphs_from_instance(msa, num_cols)
-        node_relative_pos, col_relative_pos, col_memberships = self.inference(seq_g, tf.constant(msa.seq_lens), pattern_g)
-        return node_relative_pos.numpy(), col_relative_pos.numpy(), col_memberships.numpy()
+        seq_g, col_g = self._graphs_from_instance(msa, num_cols)
+        node_relative_pos, col_relative_pos, s_nodes, s_cols = self.inference(seq_g, tf.constant(msa.seq_lens), col_g)
+        return node_relative_pos.numpy(), col_relative_pos.numpy(), s_nodes.numpy(), s_cols.numpy()
 
     def _graphs_from_instance(self, msa, num_cols):
         seq_dict = {"globals" : [np.float32(0)],
@@ -191,18 +233,18 @@ class NeuroAlignPredictor():
                         "edges" : np.zeros((len(msa.forward_senders), 1), dtype=np.float32),
                         "senders" : msa.forward_senders,
                         "receivers" : msa.forward_receivers }
-        pattern_dicts = []
+        col_dicts = []
         for i in range(num_cols):
             # col_nodes = np.zeros((msa.nodes.shape[0],1), dtype=np.float32)
             # col_nodes[sum(msa.seq_lens[:s]) + i] = 1
-            pattern_dicts.append({"globals" : [np.float32(i/num_cols)],
+            col_dicts.append({"globals" : [np.float32(i/num_cols)],
             "nodes" : msa.nodes,
             "senders" : [],
             "receivers" : [] })
         seq_g = gn.utils_tf.data_dicts_to_graphs_tuple([seq_dict])
-        pattern_g = gn.utils_tf.data_dicts_to_graphs_tuple(pattern_dicts)
-        pattern_g = gn.utils_tf.set_zero_edge_features(pattern_g, 0)
-        return seq_g, pattern_g
+        col_g = gn.utils_tf.data_dicts_to_graphs_tuple(col_dicts)
+        col_g = gn.utils_tf.set_zero_edge_features(col_g, 0)
+        return seq_g, col_g
 
 
     def load_latest(self):

NeuroAlign/Train.py
@@ -1,13 +1,13 @@
 import argparse
-import random
 import MSA
 import Model
 import Trainer
 from Config import config
+import numpy as np
 
 parser = argparse.ArgumentParser(description='Trains and tests a NeuroAlign model for the simple case of exact nucleotide matches.')
 parser.add_argument("-n", type=int, default=5000, help="number of training examples")
-parser.add_argument("-dir", type=str, default="./data_50", help="directory with data files")
+parser.add_argument("-dir", type=str, default="./data_20", help="directory with data files")
 args = parser.parse_args()
 
 #load the training dataset
@@ -22,17 +22,24 @@ predictor.load_latest()
 trainer = Trainer.NeuroAlignTrainer(config, predictor)
 
 for i in range(config["num_training_iteration"]):
-    random.shuffle(msa)
+    batch = []
+    while len(batch) < config["batch_size"]:
+        batch.append(msa[np.random.randint(len(msa))])
     train_loss_sum = 0
     l_node_rp_sum = 0
-    l_mem_sum = 0
-    for m in msa:
-        n_rp, c_rp, mem, train_loss, l_node_rp, l_col_rp, l_mem = trainer.train(m)
+    l_col_rp_sum = 0
+    score_sum = 0
+    for m in batch:
+        n_rp, c_rp, s_n, s_c, train_loss, l_node_rp, l_col_rp, score = trainer.train(m)
         train_loss_sum += train_loss.numpy()
         l_node_rp_sum += l_node_rp.numpy()
-        l_mem_sum += l_mem.numpy()
+        l_col_rp_sum += l_col_rp.numpy()
+        score_sum += score.numpy()
 
-    print(i, " l=", train_loss_sum/len(msa), "l_n=", l_node_rp_sum/len(msa), "l_mem=", l_mem_sum/len(msa))
+    print(i, " l=", train_loss_sum/config["batch_size"],
+                "l_n=", l_node_rp_sum/config["batch_size"],
+                "c_n=", l_col_rp_sum/config["batch_size"],
+                "av_score=", score_sum/config["batch_size"])
 
     if i % config["savestate_milestones"] == 0 and i > 0:
         predictor.save()

NeuroAlign/Trainer.py
@@ -12,17 +12,19 @@ class NeuroAlignTrainer():
         def train_step(sequence_graph, seq_lens, col_priors,
                         target_node_rp, target_col_segment_ids):
             with tf.GradientTape() as tape:
-                n_rp, c_rp, mem = self.predictor.model(sequence_graph, seq_lens, col_priors, config["train_mp_iterations"])
+                n_rp, c_rp, s_n, s_c = self.predictor.model(sequence_graph, seq_lens, col_priors, config["train_mp_iterations"])
                 l_node_rp = tf.compat.v1.losses.mean_squared_error(target_node_rp, n_rp)
                 l_col_rp = tf.compat.v1.losses.mean_squared_error(col_priors.globals, c_rp)
-                l_mem = tf.math.unsorted_segment_prod(mem, target_col_segment_ids, gn.utils_tf.get_num_graphs(col_priors))
-                l_mem = tf.reduce_mean(l_mem)
+                # score_s_n = tf.math.unsorted_segment_prod(s_n, target_col_segment_ids, gn.utils_tf.get_num_graphs(col_priors))
+                # score_s_c = tf.math.unsorted_segment_prod(s_c, target_col_segment_ids, gn.utils_tf.get_num_graphs(col_priors))
+                # score = tf.reduce_mean(score_s_n) + tf.reduce_mean(score_s_c)
+                score = tf.reduce_mean(tf.math.unsorted_segment_prod(tf.sqrt(s_n*s_c), target_col_segment_ids, gn.utils_tf.get_num_graphs(col_priors)))
                 regularizer = snt.regularizers.L2(config["l2_regularization"])
-                train_loss = l_node_rp + l_col_rp - l_mem + regularizer(self.predictor.model.trainable_variables)
+                train_loss = l_node_rp + l_col_rp - score + regularizer(self.predictor.model.trainable_variables)
 
                 gradients = tape.gradient(train_loss, self.predictor.model.trainable_variables)
                 optimizer.apply(gradients, self.predictor.model.trainable_variables)
-                return n_rp, c_rp, mem, train_loss, l_node_rp, l_col_rp, l_mem
+                return n_rp, c_rp, s_n, s_c, train_loss, l_node_rp, l_col_rp, score
 
         # Get the input signature for that function by obtaining the specs
         self.input_signature = [