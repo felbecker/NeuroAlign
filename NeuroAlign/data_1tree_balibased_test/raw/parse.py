b17ae1bbdefa2963fc927f85c4cf4d2170a92dbc
Ung0d <beckerfelix94@gmail.com>
2020-05-04 18:54:53 +0200

    new data, balibase 1 tree

---
+++ NeuroAlign/data_1tree_balibased_test/raw/parse.py
@@ -0,0 +1,22 @@
+import sys
+
+
+with open(sys.argv[1]+"_TRUE.phy", "r") as file:
+    num_data = 1
+    skipped_header = False
+    out = open("../A"+"{0:0=4d}".format(num_data)+".fa", "w")
+    for line in file:
+        splitln = line.split()
+        if len(splitln)==0:
+            if skipped_header:
+                out.close()
+                skipped_header = False
+                num_data += 1
+                out = open("../A"+"{0:0=4d}".format(num_data)+".fa", "w")
+        else:
+            if not skipped_header:
+                skipped_header = True
+            else:
+                out.write(">" + splitln[0] + "\n")
+                out.write(splitln[1] + "\n")
+    out.close()
