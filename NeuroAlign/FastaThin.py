b7358e5e8ba77c78f05eb4a3e6e5885ea1d022d0
Ung0d <beckerfelix94@gmail.com>
2020-09-16 15:22:22 +0200

    seq pre model

--- NeuroAlign/FastaThin.py
+++ NeuroAlign/FastaThin.py
@@ -4,6 +4,8 @@ import numpy as np
 parser = argparse.ArgumentParser(description='Thins out a fasta file, producing a new file with only xx % of the sequences.')
 parser.add_argument("-f", type=str, default="", help="fasta file")
 parser.add_argument("-thin", type=np.float32, default="", help="thinning probability")
+parser.add_argument("-minlen", type=int, default=0, help="discards all sequences below this length")
+parser.add_argument("-maxlen", type=int, default=100000, help="discards all sequences above this length")
 parser.add_argument("-out", type=str, default="", help="output file")
 args = parser.parse_args()
 
@@ -11,14 +13,22 @@ args = parser.parse_args()
 with open(args.f) as f:
     content = f.readlines()
 
+def write_batch(out, batch):
+    l = sum([len(l) for l in batch[1:]])
+    if l > args.minlen and l < args.maxlen:
+        out.write("".join(batch))
+
 with open(args.out, "w") as out:
     write = False
     for i,line in enumerate(content):
         if len(line)>0:
-            if line[0]=='>':
+            if line[0]=='>': #header line
+                if write:
+                    write_batch(out, batch)
+                batch = []
                 write = np.random.rand() < args.thin
-                if write:
-                    out.write(line)
-            else:
-                if write:
-                    out.write(line)
+                batch.append(line)
+            else: #take into account that the sequence may occupy multiple lines in the file
+                batch.append(line)
+                if i == len(content)-1 and write:
+                    write_batch(out, batch)
--- NeuroAlign/SequenceModel.py
+++ NeuroAlign/SequenceModel.py
@@ -5,14 +5,17 @@ import numpy as np
 import MSA
 
 
-SITE_DIM = 25
-LSTM_DIM = 32
+LSTM_DIM = 256
 ALPHABET = ['A', 'R',  'N',  'D',  'C',  'Q',  'E',  'G',  'H', 'I',  'L',  'K',  'M',  'F',  'P', 'S',  'T',  'W',  'Y',  'V',  'B',  'Z',  'X']
 
 LEARNING_RATE = 1e-3
-NUM_EPOCHS = 10
-BATCH_SIZE = 10
-VALIDATION_SPLIT = 0.1
+NUM_EPOCHS = 100
+BATCH_SIZE = 256
+VALIDATION_SPLIT = 0.05
+
+
+##################################################################################################
+##################################################################################################
 
 #load the data
 msa = MSA.Instance("Pfam_very_thin.fasta", ALPHABET, gaps=False)
@@ -32,44 +35,67 @@ train, test = np.split(indices, [int(len(msa.raw_seq)*(1-VALIDATION_SPLIT))])
 ##################################################################################################
 
 #provides batches of protein sequences
-class SequenceBatchGenerator(object):
-    def __init__(self, split, batch_size):
-        self.data = data
+class SequenceBatchGenerator(tf.keras.utils.Sequence):
+    def __init__(self, split):
         self.split = split
-        self.batch_size = batch_size
 
-    def generate(self):
-        while True:
-            batch = msa.raw_seq[ np.random.randint(self.split, size=self.batch_size) ]
-            num_steps = max([s.shape[0] for s in msa.raw_seq[batch]])
-            x = np.zeros((self.batch_size, num_steps, len(ALPHABET)))
-            y = np.zeros((self.batch_size, num_steps, len(ALPHABET)))
-            yield x, y
+    def __len__(self):
+        return int(np.floor(len(self.split) / BATCH_SIZE)) #steps per epoch
+
+    def __getitem__(self, index):
+        batch_indices = np.random.choice(self.split, size=BATCH_SIZE)
+        batch = [msa.raw_seq[i] for i in batch_indices]
+        batch_lens = [s.shape[0] for s in batch]
+        num_steps = max(batch_lens)
+        seq = np.zeros((BATCH_SIZE, num_steps, len(ALPHABET)), dtype=np.float32)
+        for j,(l,s) in enumerate(zip(batch_lens, batch)):
+            seq[j,np.arange(l),s] = 1
+        return seq, seq, [None]
 
 
-train_gen = SequenceBatchGenerator(train, BATCH_SIZE)
-val_gen = SequenceBatchGenerator(test, BATCH_SIZE)
+train_gen = SequenceBatchGenerator(train)
+val_gen = SequenceBatchGenerator(test)
 
 ##################################################################################################
 ##################################################################################################
 
 #embedding layer that converts nucleotide one hot encodings to corresponding variables
-class SequenceEmbedding(keras.layers.Layer):
+class SequenceEmbedding(tf.keras.layers.Layer):
     def __init__(self):
-        super(IntraSNPLayer, self).__init__()
+        super(SequenceEmbedding, self).__init__()
 
     def build(self, input_shape):
-        self.sites_w = self.add_weight(shape=(len(ALPHABET), SITE_DIM), trainable=True)
+        self.sites_w = self.add_weight(shape=(len(ALPHABET), len(ALPHABET)),
+                                        trainable=True,
+                                        initializer = tf.constant_initializer(np.eye(len(ALPHABET))))
 
     #input dim is [batchsize, seq_len, len(ALPHABET)]
     def call(self, inputs):
         return tf.linalg.matmul(inputs, self.sites_w)
 
+
+#shift such that output i contains forward pass to i-1 and backward pass to i+1 (zero at edges)
+#-> output i has never seen position i (which it will predict)
+class OutputShift(tf.keras.layers.Layer):
+    def __init__(self):
+        super(OutputShift, self).__init__()
+
+    #input dim is [batchsize, seq_len, len(ALPHABET)]
+    def call(self, inputs):
+        #inputs have shape (batch, len, 2*LSTM_DIM)
+        #shift such that:
+        #out_i = (forward_i-1 , backward_i+1)
+        zeros = tf.zeros_like(inputs[:,0:1,:LSTM_DIM])
+        forward_shifted = tf.concat([zeros, inputs[:,1:,:LSTM_DIM]], axis=1)
+        backward_shifted = tf.concat([inputs[:,:-1,LSTM_DIM:], zeros], axis=1)
+        output = tf.concat([forward_shifted, backward_shifted], axis=-1)
+        return output
+
 ##################################################################################################
 ##################################################################################################
 
 #build the model
-model = Sequential()
+model = tf.keras.Sequential()
 
 #compute the initial embeddings per sequence symbol
 model.add(SequenceEmbedding())
@@ -78,13 +104,16 @@ model.add(SequenceEmbedding())
 model.add(tf.keras.layers.Masking(mask_value=0.0))
 
 #bidirectional LSTMs
-model.add(tf.keras.layers.Bidirectional(layer = LSTM(LSTM_DIM, return_sequences=True),
-                                        backward_layer = LSTM(LSTM_DIM, return_sequences=True)))
-model.add(tf.keras.layers.Bidirectional(layer = LSTM(LSTM_DIM, return_sequences=True),
-                                        backward_layer = LSTM(LSTM_DIM, return_sequences=True)))
+model.add(tf.keras.layers.Bidirectional(layer = tf.keras.layers.LSTM(LSTM_DIM, return_sequences=True),
+                                        backward_layer = tf.keras.layers.LSTM(LSTM_DIM, return_sequences=True, go_backwards=True),
+                                        merge_mode = "concat"))
+model.add(tf.keras.layers.Bidirectional(layer = tf.keras.layers.LSTM(LSTM_DIM, return_sequences=True),
+                                        backward_layer = tf.keras.layers.LSTM(LSTM_DIM, return_sequences=True, go_backwards=True),
+                                        merge_mode = "concat"))
 
 #transform to symbol probabilities, this part will be skipped when shipping the model
-model.add(tf.keras.layers.TimeDistributed(Dense(len(ALPHABET))))
+model.add(OutputShift())
+model.add(tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(len(ALPHABET))))
 model.add(tf.keras.layers.Activation('softmax'))
 
 ##################################################################################################
@@ -94,6 +123,9 @@ model.compile(loss='categorical_crossentropy',
                 optimizer=tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE),
                 metrics=['categorical_accuracy'])
 
-model.fit(train_gen.generate(), validation_data=val_gen.generate(),
-            batch_size = 1, epochs = NUM_EPOCHS, verbose = 2,
-            callbacks=[checkpointer])
+data = val_gen.__getitem__(0)
+
+model.fit(train_gen,
+            validation_data=val_gen,
+            epochs = NUM_EPOCHS,
+            verbose = 1)
