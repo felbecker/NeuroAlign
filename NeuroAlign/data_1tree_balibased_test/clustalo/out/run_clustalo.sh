95389ff2e605647e0c379014728e54cb04701e71
Ung0d <beckerfelix94@gmail.com>
2020-09-29 16:01:39 +0200

    removed data

--- NeuroAlign/data_1tree_balibased_test/clustalo/out/run_clustalo.sh
+++
@@ -1,4 +0,0 @@
-for i in `seq 1 1000`
-do
-  clustalo --infile=$(printf "../A%04d.fa" "$i") --seqtype=PROTEIN --outfile=$(printf "A%04d.fa" "$i") --outfmt=fa --threads=4 -v
-done
--- NeuroAlign/data_bali_et_al/ali2fasta.sh
+++
@@ -1,4 +0,0 @@
-#converts all ali files in the directory to fasta files with the same name
-for i in *.ali; do
-  python3 ali2fasta.py -f="$i"
-done
