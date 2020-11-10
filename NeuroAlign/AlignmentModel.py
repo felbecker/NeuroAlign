c73fd6f2d9c2fdb2e627a13ea1c45fb069e36ca4
Ung0d <beckerfelix94@gmail.com>
2020-11-10 13:24:30 +0100

    pca based visualization of seq embeddings

--- NeuroAlign/AlignmentModel.py
+++ NeuroAlign/AlignmentModel.py
@@ -12,30 +12,32 @@ jobid = 777#int(os.getenv('SLURM_ARRAY_TASK_ID'))
 
 ALPHABET = ['A', 'R',  'N',  'D',  'C',  'Q',  'E',  'G',  'H', 'I',  'L',  'K',  'M',  'F',  'P', 'S',  'T',  'W',  'Y',  'V',  'B',  'Z',  'X', 'U', 'O']
 
+
 ##################################################################################################
 ##################################################################################################
 
 #number of message passing iterations to perform
-NUM_ITERATIONS = 4
+NUM_ITERATIONS = 5
 
 #the dimensions for the different internal representations
-SITE_DIM = 32
-SEQ_LSTM_DIM = 300
+SITE_DIM = 50
+SEQ_LSTM_DIM = 1000
 CONS_LSTM_DIM = 300
 
 #hidden layer sizes for the MLPs
-ENCODER_LAYERS = [100]
-SEQUENCE_MSGR_LAYERS = [200, 200]
-CONSENSUS_MSGR_LAYERS = [200, 200]
+ENCODER_LAYERS = [200]
+SEQUENCE_MSGR_LAYERS = [300, 300]
+CONSENSUS_MSGR_LAYERS = [300, 300]
 
 #if false, each message passing iteration uses unique parameters
 SHARED_ITERATIONS = True#True if jobid-1 < 4 else False
 
 #if true, total loss is the sum of losses from all iterations
-LOSS_OVER_ALL_ITERATIONS = False
+LOSS_OVER_ALL_ITERATIONS = True
 
-#if true, a seconary loss term for gap lengths between all pairs of sites is used for training
-USE_GAP_MULTI_TASK_LOSS = True
+# f true, seconary loss terms for gap lengths between all pairs of sites and
+# for column aminoacid distributions decoded from consensus are used for training
+USE_MULTI_TASK_LOSSES = False
 
 
 #percentage of families not used for training
@@ -45,7 +47,7 @@ VALIDATION_SPLIT = 0.05#0.01
 #maximum number of sites in a batch
 #must be at least as large as the sum of the two longest sequences in all families
 
-BATCH_SIZE = 4000
+BATCH_SIZE = 1200
 
 #number of splits for the membership updates
 #this step is typically the memory-bottleneck of the model
@@ -56,27 +58,28 @@ BATCH_SIZE = 4000
 #this parameter has no effect during training
 COL_BATCHES = 1
 
-LEARNING_RATE = 1e-4
+LEARNING_RATE = 4e-5
 
-NUM_EPOCHS = 20
+NUM_EPOCHS = 40
 
-NAME = "alignment_model"#+str(jobid-1)
+NAME = "alignment_model"
+CHECKPOINT_PATH = NAME+"/model.ckpt"
 
-SEQ_IN_DIM = len(ALPHABET)
+SEQ_IN_DIM = len(ALPHABET)+2
 
 __POS_WEIGHT = [40,90,40,90]
-POS_WEIGHT = 10#__POS_WEIGHT[(jobid-1)%4]#70#155.5
-NEG_WEIGHT = 1#0.5
+POS_WEIGHT = 80#__POS_WEIGHT[(jobid-1)%4]#70#155.5
+NEG_WEIGHT = 0.1#0.5
 
 __LAST_ITERATION_WEIGHT = [1,1,8,8]
 LAST_ITERATION_WEIGHT = 1#__LAST_ITERATION_WEIGHT[(jobid-1)%4]
 
-MEM_LOSS_WEIGHT = 0.5
 
-##################################################################################################
-##################################################################################################
+MEM_LOSS_WEIGHT = 1
+GAP_LOSS_WEIGHT = 0.1
+AA_DIST_LOSS_WEIGHT = 1
 
-CHECKPOINT_PATH = NAME+"/model.ckpt"
+COLUMN_OVEREXTENSION = 1
 
 ##################################################################################################
 ##################################################################################################
@@ -90,8 +93,7 @@ CFG_TXT = ("NeuroAlign config  \n" +
       " - sequence messenger: "+ str(SEQUENCE_MSGR_LAYERS)+
       " - consensus messenger: "+ str(CONSENSUS_MSGR_LAYERS)+" \n"+
       " - shared iterations: "+ str(SHARED_ITERATIONS)+
-      " - loss over all iterations: "+ str(LOSS_OVER_ALL_ITERATIONS)+
-      " - secondary gap loss: "+ str(USE_GAP_MULTI_TASK_LOSS)+ " \n"+
+      " - loss over all iterations: "+ str(LOSS_OVER_ALL_ITERATIONS)+" \n"+
       " - pos weight: "+ str(POS_WEIGHT)+
       " - neg weight: "+ str(NEG_WEIGHT)+
       " - batch: "+ str(BATCH_SIZE)+
@@ -130,26 +132,48 @@ class MLP(layers.Layer):
 ##################################################################################################
 ##################################################################################################
 
-#decodes all combinations of site (resp. consensus) and column representations
-#to soft memberships
+#decodes all combinations of sites and consensus to a soft alignment matrix
 class MembershipDecoder(layers.Layer):
     def __init__(self):
         super(MembershipDecoder, self).__init__()
 
     def build(self, input_shape):
-        self.softmax = layers.Activation("softmax")
+        self.in_sites = layers.Dense(32, activation="relu")
+        self.in_cons = layers.Dense(32, activation="relu")
+        self.decoder = MLP([32])
+        self.out_transf = layers.Dense(1)
 
     def call(self, inputs):
-        if type(inputs) is not list and len(inputs) != 2:
-            raise Exception('MembershipDecoder must be called on a list of exactly 2 tensors ')
-        sites = tf.expand_dims(inputs[0], 1)
-        n_batch_size = tf.cast(tf.math.ceil(tf.cast(tf.shape(inputs[1])[0], dtype=tf.float32)/COL_BATCHES), dtype=tf.int32)
-        norm = []
-        for i in range(COL_BATCHES):
-            consensus = tf.expand_dims(inputs[1][(i*n_batch_size):((i+1)*n_batch_size)], 0)
-            norm.append(tf.norm(sites - consensus, ord=1, axis=-1))
-        norm = tf.concat(norm, axis=1)
-        return self.softmax(-norm)
+        if type(inputs) is not list and len(inputs) != 6:
+            raise Exception('MembershipDecoder must be called on a list of exactly 6 tensors ')
+
+        sites = self.in_sites(inputs[0])
+        cons = self.in_cons(inputs[1])
+
+        sites = tf.expand_dims(sites, 1)
+        cons = tf.expand_dims(cons, 0)
+
+        comb = tf.reshape(sites + cons, (-1, 32))
+
+        logits = self.out_transf(self.decoder(comb))
+        logits = tf.reshape(logits, (tf.shape(inputs[0])[0], tf.shape(inputs[1])[0]))
+
+        M = tf.nn.softmax(logits, axis=-1)
+
+        M_c_lower = tf.cumsum(M, axis=-1, reverse=True)
+        M_c_lower = tf.scatter_nd(tf.expand_dims(inputs[2], -1), M_c_lower, shape=(inputs[4] * inputs[5], tf.shape(inputs[1])[0]))
+        M_c_lower = tf.reshape(M_c_lower, (inputs[4], inputs[5], tf.shape(inputs[1])[0]))
+        M_lower = tf.math.cumprod(tf.nn.relu(1 - M_c_lower), axis=1, exclusive=True)
+
+        M_c_higher = tf.cumsum(M, axis=-1)
+        M_c_higher = tf.scatter_nd(tf.expand_dims(inputs[3], -1), M_c_higher, shape=(inputs[4] * inputs[5], tf.shape(inputs[1])[0]))
+        M_c_higher = tf.reshape(M_c_higher, (inputs[4], inputs[5], tf.shape(inputs[1])[0]))
+        M_higher = tf.math.cumprod(tf.nn.relu(1 - M_c_higher), axis=1, exclusive=True)
+
+        M_lower = tf.gather(tf.reshape(M_lower, (-1, tf.shape(inputs[1])[0]) ), inputs[2], axis=0)
+        M_higher = tf.gather(tf.reshape(M_higher, (-1, tf.shape(inputs[1])[0]) ), inputs[3], axis=0)
+
+        return M * M_lower * M_higher
 
 ##################################################################################################
 ##################################################################################################
@@ -160,11 +184,36 @@ class GapDecoder(layers.Layer):
     def __init__(self):
         super(GapDecoder, self).__init__()
 
-    def call(self, M):
-        col_range = tf.expand_dims(tf.range(tf.shape(M)[-1], dtype=M.dtype), 0)
-        soft_argmax = tf.reduce_sum(M * col_range, axis=-1)
-        gaps = tf.expand_dims(soft_argmax, 0) - tf.expand_dims(soft_argmax, 1)
-        return gaps/tf.cast(tf.shape(M)[-1], dtype=M.dtype)
+    def build(self, input_shape):
+        self.in_sites = layers.Dense(32, activation="relu")
+        self.out_transf = layers.Dense(1)
+
+    def call(self, gathered_sequences):
+        seq = self.in_sites(gathered_sequences)
+        gaps = tf.expand_dims(seq, 1) + tf.expand_dims(seq, 0)
+        gaps = tf.reshape(gaps, (-1, 32))
+        gaps = self.out_transf(gaps)
+        gaps = tf.reshape(gaps, (tf.shape(gathered_sequences)[0], tf.shape(gathered_sequences)[0]))
+        return gaps
+
+##################################################################################################
+##################################################################################################
+
+#decodes amino acid distributions from columns (consensus sequence)
+class AminoDistDecoder(layers.Layer):
+    def __init__(self):
+        super(AminoDistDecoder, self).__init__()
+
+    def build(self, input_shape):
+        self.in_cons = layers.Dense(32, activation="relu")
+        self.out_transf = layers.Dense(len(ALPHABET))
+
+    def call(self, inputs):
+        if type(inputs) is not list and len(inputs) != 2:
+            raise Exception('AminoDistDecoder must be called on a list of exactly 2 tensors ')
+        aa_dist = self.out_transf(self.in_cons(inputs[0]))
+        aa_dist = tf.nn.softmax(aa_dist, axis=-1) * tf.expand_dims(tf.reduce_sum(inputs[1], axis=0), -1)
+        return aa_dist
 
 ##################################################################################################
 ##################################################################################################
@@ -186,25 +235,28 @@ class Messenger(layers.Layer):
         return messages_to_cons
 
 ##################################################################################################
-##################################################################################################
+################################4/5wENU41iG-fcZ19h08AXYxWtk4vAywDxkcqvQdH_ucPW8ODHX42dicA##################################################################
 
-def make_model(training = True):
+def make_model(training = True, output_representations = False):
     #define inputs
     sequences = keras.Input(shape=(None,SEQ_IN_DIM), name="sequences")
     sequence_gather_indices = keras.Input(shape=(), name="sequence_gather_indices", dtype=tf.int32)
+    rev_sequence_gather_indices = keras.Input(shape=(), name="rev_sequence_gather_indices", dtype=tf.int32)
     initial_memberships = keras.Input(shape=(None,), name="initial_memberships")
 
     encoder = MLP(ENCODER_LAYERS+[SITE_DIM])
     mem_decoder = MembershipDecoder()
-    if USE_GAP_MULTI_TASK_LOSS:
+    if USE_MULTI_TASK_LOSSES:
         gap_decoder = GapDecoder()
+        aa_dist_decoder = AminoDistDecoder()
+
     #networks
     if SHARED_ITERATIONS:
         seq_lstm = [layers.Bidirectional(layers.LSTM(SEQ_LSTM_DIM, return_sequences=True),
                                         backward_layer=layers.LSTM(SEQ_LSTM_DIM, return_sequences=True, go_backwards=True))]*NUM_ITERATIONS
         seq_dense = [layers.TimeDistributed(layers.Dense(SITE_DIM))]*NUM_ITERATIONS
         cons_lstm = [layers.Bidirectional(layers.LSTM(CONS_LSTM_DIM, return_sequences=True),
-                                        backward_layer=layers.LSTM(SEQ_LSTM_DIM, return_sequences=True, go_backwards=True))]*NUM_ITERATIONS
+                                        backward_layer=layers.LSTM(CONS_LSTM_DIM, return_sequences=True, go_backwards=True))]*NUM_ITERATIONS
         cons_dense = [layers.TimeDistributed(layers.Dense(2*SITE_DIM))]*NUM_ITERATIONS
         seq_to_cons_messenger = [Messenger(SEQUENCE_MSGR_LAYERS)]*NUM_ITERATIONS
         cons_to_seq_messenger = [Messenger(CONSENSUS_MSGR_LAYERS)]*NUM_ITERATIONS
@@ -213,7 +265,7 @@ def make_model(training = True):
                                         backward_layer=layers.LSTM(SEQ_LSTM_DIM, return_sequences=True, go_backwards=True)) for _ in range(NUM_ITERATIONS)]
         seq_dense = [layers.TimeDistributed(layers.Dense(SITE_DIM)) for _ in range(NUM_ITERATIONS)]
         cons_lstm = [layers.Bidirectional(layers.LSTM(CONS_LSTM_DIM, return_sequences=True),
-                                        backward_layer=layers.LSTM(SEQ_LSTM_DIM, return_sequences=True, go_backwards=True)) for _ in range(NUM_ITERATIONS)]
+                                        backward_layer=layers.LSTM(CONS_LSTM_DIM, return_sequences=True, go_backwards=True)) for _ in range(NUM_ITERATIONS)]
         cons_dense = [layers.TimeDistributed(layers.Dense(2*SITE_DIM)) for _ in range(NUM_ITERATIONS)]
         seq_to_cons_messenger = [Messenger(SEQUENCE_MSGR_LAYERS) for _ in range(NUM_ITERATIONS)]
         cons_to_seq_messenger = [Messenger(CONSENSUS_MSGR_LAYERS) for _ in range(NUM_ITERATIONS)]
@@ -225,20 +277,19 @@ def make_model(training = True):
     gathered_sequences = gathered_initial_sequences
 
     #initial consensus
+    #av_site = tf.reduce_mean(gathered_sequences, axis=0, keepdims=True)
+    #av_site = tf.concat([av_site, av_site], axis=-1)
+    #consensus = tf.tile(av_site, [tf.shape(initial_memberships)[1], 1])
     #consensus = tf.zeros((tf.shape(initial_memberships)[1], 2*SITE_DIM))
-    consensus = layers.Dense(2*SITE_DIM)(tf.expand_dims(tf.range(tf.shape(initial_memberships)[1], dtype=tf.float32), 1))
+    consensus = layers.Dense(2*SITE_DIM)(tf.expand_dims(tf.range(tf.shape(initial_memberships)[1], dtype=tf.float32) / tf.cast(tf.shape(initial_memberships)[1], dtype=tf.float32), 1))
 
-    M = mem_decoder([layers.Concatenate()([gathered_initial_sequences, gathered_sequences]), consensus])
+    gathered_sequences_Concat = layers.Concatenate()([gathered_initial_sequences, gathered_sequences])
+    M = mem_decoder([gathered_sequences_Concat, consensus, sequence_gather_indices, rev_sequence_gather_indices, tf.shape(sequences)[0], tf.shape(sequences)[1]])
+    #consensus = tf.matmul(M, gathered_sequences_Concat, transpose_a=True)
 
     outputs = []
 
     for i in range(NUM_ITERATIONS):
-
-        gathered_sequences_Concat = layers.Concatenate()([gathered_initial_sequences, gathered_sequences])
-        messages_seq_to_cons = seq_to_cons_messenger[i]([gathered_sequences_Concat, tf.transpose(M)])
-        concat_cons = layers.Concatenate()([consensus, messages_seq_to_cons])
-        consensus = cons_dense[i](cons_lstm[i](tf.expand_dims(concat_cons, axis=0)))
-        consensus = tf.squeeze(consensus, axis=0)
 
         messages_cons_to_seq = cons_to_seq_messenger[i]([consensus, M])
         messages_cons_to_seq = tf.scatter_nd(tf.expand_dims(sequence_gather_indices, -1), messages_cons_to_seq,
@@ -248,22 +299,34 @@ def make_model(training = True):
         encoded_sequences = seq_dense[i](seq_lstm[i](concat_sequences)) #will always mask
         gathered_sequences = tf.gather(tf.reshape(encoded_sequences, (-1, SITE_DIM) ), sequence_gather_indices, axis=0)
 
-        M = mem_decoder([layers.Concatenate()([gathered_initial_sequences, gathered_sequences]), consensus])
+        gathered_sequences_Concat = layers.Concatenate()([gathered_initial_sequences, gathered_sequences])
+
+        messages_seq_to_cons = seq_to_cons_messenger[i]([gathered_sequences_Concat, tf.transpose(M)])
+        concat_cons = layers.Concatenate()([consensus, messages_seq_to_cons])
+        consensus = cons_dense[i](cons_lstm[i](tf.expand_dims(concat_cons, 0)))
+        consensus = tf.squeeze(consensus, axis=0)
+        #consensus = cons_network[i](concat_cons)
+
+        M = mem_decoder([gathered_sequences_Concat, consensus, sequence_gather_indices, rev_sequence_gather_indices, tf.shape(sequences)[0], tf.shape(sequences)[1]])
 
         if training:
             if LOSS_OVER_ALL_ITERATIONS:
                     outputs.append(layers.Lambda(lambda x: x, name="mem"+str(i))(tf.linalg.matmul(M, M, transpose_b=True)))
-                    if USE_GAP_MULTI_TASK_LOSS:
-                        outputs.append(layers.Lambda(lambda x: x, name="gap"+str(i))(gap_decoder(M)))
+                    if USE_MULTI_TASK_LOSSES:
+                        outputs.append(layers.Lambda(lambda x: x, name="gap"+str(i))(gap_decoder(gathered_sequences_Concat)))
+                        outputs.append(layers.Lambda(lambda x: x, name="aa_dist"+str(i))(aa_dist_decoder([consensus, M])))
             elif i == (NUM_ITERATIONS-1):
                     outputs.append(layers.Lambda(lambda x: x, name="mem")(tf.linalg.matmul(M, M, transpose_b=True)))
-                    if USE_GAP_MULTI_TASK_LOSS:
-                        outputs.append(layers.Lambda(lambda x: x, name="gap")(gap_decoder(M)))
+                    if USE_MULTI_TASK_LOSSES:
+                        outputs.append(layers.Lambda(lambda x: x, name="gap")(gap_decoder(gathered_sequences_Concat)))
+                        outputs.append(layers.Lambda(lambda x: x, name="aa_dist")(aa_dist_decoder([consensus, M])))
         else:
             outputs.append(M)
+            if output_representations:
+                outputs.extend([gathered_sequences_Concat, consensus])
 
     model = keras.Model(
-        inputs=[sequences, sequence_gather_indices, initial_memberships],
+        inputs=[sequences, sequence_gather_indices, rev_sequence_gather_indices, initial_memberships],
         outputs=outputs
     )
 
--- NeuroAlign/CheckAlignmentModel.py
+++ NeuroAlign/CheckAlignmentModel.py
@@ -6,18 +6,39 @@ import Postprocessing
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
+import time
+from sklearn.decomposition import PCA
+
+
+################################################################################################################################################
+################################################################################################################################################
+
+MAX_SEQ = 8
+
+################################################################################################################################################
+################################################################################################################################################
 
 parser = argparse.ArgumentParser(description='Predicts a complete alignment given a fasta file with raw sequences.')
 parser.add_argument("-f", type=str, default="", help="input fasta file")
 parser.add_argument("-o", type=str, default="", help="output alignment fasta file")
+parser.add_argument("-vis_mem", action='store_true', help="output a membership heatmap for max the first MAX_SEQ sequences and all iterations")
+parser.add_argument("-vis_seq_rep", action='store_true', help="visualize the representations of each sequence site")
 args = parser.parse_args()
 
-al_model = AlignmentModel.make_model(training = False)
+################################################################################################################################################
+################################################################################################################################################
+
+al_model = AlignmentModel.make_model(training = False, output_representations = args.vis_seq_rep)
 if os.path.isfile(AlignmentModel.CHECKPOINT_PATH+".index"):
     al_model.load_weights(AlignmentModel.CHECKPOINT_PATH)
 else:
     print("Could not load alignment model.", flush=True)
     quit()
+
+################################################################################################################################################
+################################################################################################################################################
+
+start_time = time.time()
 
 msa = MSA.Instance(args.f, AlignmentModel.ALPHABET, gaps = True, contains_lower_case = True)
 
@@ -26,30 +47,56 @@ if not msa.valid:
     quit()
 
 maxlen = max(msa.seq_lens)
+num_columns = max(msa.seq_lens)*AlignmentModel.COLUMN_OVEREXTENSION
 
 #one-hot encode sequences
-seq = np.zeros((len(msa.raw_seq), maxlen, len(AlignmentModel.ALPHABET)), dtype=np.float32)
+seq = np.zeros((len(msa.raw_seq), maxlen, len(AlignmentModel.ALPHABET)+2), dtype=np.float32)
 for j,(l,si) in enumerate(zip(msa.seq_lens, msa.raw_seq)):
     lrange = np.arange(l)
     seq[j,lrange,si] = 1
+    seq[j,lrange,len(AlignmentModel.ALPHABET)] = lrange / num_columns
+    seq[j,lrange,len(AlignmentModel.ALPHABET)+1] = l / num_columns
 
 #maps padded sequences to unpadded
 sequence_gather_indices = np.concatenate([(i*maxlen + np.arange(l)) for i,l in enumerate(msa.seq_lens)], axis=0)
 sequence_gather_indices = sequence_gather_indices.astype(np.int32)
-
-num_columns = max(msa.seq_lens)*2
+rev_sequence_gather_indices = np.concatenate([(i*maxlen + np.flip(np.arange(l))) for i,l in enumerate(msa.seq_lens)], axis=0)
+rev_sequence_gather_indices = rev_sequence_gather_indices.astype(np.int32)
 
 initial_memberships = np.ones((sum(msa.seq_lens), int(num_columns))) / num_columns
 
 input_dict = {  "sequences" : seq,
                 "sequence_gather_indices" : sequence_gather_indices,
+                "rev_sequence_gather_indices" : rev_sequence_gather_indices,
                 "initial_memberships" : initial_memberships }
 
-mem = al_model(input_dict)
+out = al_model(input_dict)
+
+if args.vis_seq_rep:
+    mem = [out[3*i] for i in range(int(len(out)/3))]
+    seq_rep = [out[3*i+1] for i in range(int(len(out)/3))]
+    cons_rep = [out[3*i+2] for i in range(int(len(out)/3))]
+else:
+    if AlignmentModel.NUM_ITERATIONS == 1:
+        mem = [out]
+    else:
+        mem = out
 
 cols = Postprocessing.seq_consistent(msa, mem[-1])
+
+end_time = time.time()
+
 print("prec, recall: ", msa.recall_prec(cols))
-#MSA.column_pred_to_fasta(msa, cols, args.o)
+print("It took ", end_time - start_time, " seconds to construct the alignment.")
+
+################################################################################################################################################
+################################################################################################################################################
+
+if args.o != "":
+    MSA.column_pred_to_fasta(msa, cols, args.o)
+
+################################################################################################################################################
+################################################################################################################################################
 
 def plot_heatmap_memberships(msa, mem):
 
@@ -64,7 +111,9 @@ def plot_heatmap_memberships(msa, mem):
     fontsize = 10
 
     for i, r  in enumerate(ref_splits):
-        ax = plt.subplot(len(msa.raw_seq),AlignmentModel.NUM_ITERATIONS+1,i*(AlignmentModel.NUM_ITERATIONS+1)+1)
+        if i == MAX_SEQ:
+            break
+        ax = plt.subplot(min(len(msa.raw_seq), MAX_SEQ),AlignmentModel.NUM_ITERATIONS+1,i*(AlignmentModel.NUM_ITERATIONS+1)+1)
         if i == 0:
             ax.set_title("Reference", fontsize=fontsize)
         ax.get_xaxis().set_ticks([])
@@ -75,7 +124,7 @@ def plot_heatmap_memberships(msa, mem):
         plt.imshow(r, cmap='hot', interpolation='nearest')
 
         for j in range(AlignmentModel.NUM_ITERATIONS):
-            ax = plt.subplot(len(msa.raw_seq),AlignmentModel.NUM_ITERATIONS+1,i*(AlignmentModel.NUM_ITERATIONS+1)+2+j)
+            ax = plt.subplot(min(len(msa.raw_seq), MAX_SEQ),AlignmentModel.NUM_ITERATIONS+1,i*(AlignmentModel.NUM_ITERATIONS+1)+2+j)
             if i == 0:
                 ax.set_title("Iteration {}".format(j+1), fontsize=fontsize)
             ax.get_xaxis().set_visible(False)
@@ -83,4 +132,33 @@ def plot_heatmap_memberships(msa, mem):
             plt.imshow(mem_splits[j][i], cmap='hot', interpolation='nearest')
     plt.show()
 
-#plot_heatmap_memberships(msa, mem)
+if args.vis_mem:
+    plot_heatmap_memberships(msa, mem)
+
+################################################################################################################################################
+################################################################################################################################################
+
+if args.vis_seq_rep:
+    seq_rep = out[-2]
+    consensus = out[-1]
+    pca = PCA(n_components=2)
+    seq_2d = pca.fit_transform(seq_rep)
+    cmap = plt.cm.get_cmap('viridis', len(msa.seq_lens))
+    colors = []
+    for i,l in enumerate(msa.seq_lens):
+        colors.extend([cmap(i)]*l)
+
+    x_range = np.amax(seq_2d[:,0]) - np.amin(seq_2d[:,0])
+    y_range = np.amax(seq_2d[:,1]) - np.amin(seq_2d[:,1])
+    anno_offset = (x_range*0.004, y_range*0.004)
+
+    #for s in msa.raw_seq:
+        #colors.extend([cmap(aa) for aa in s])
+    plt.scatter(seq_2d[:,0], seq_2d[:,1], color=colors)
+    lsum = 0
+    for s,l in zip(msa.raw_seq, msa.seq_lens):
+        for i in range(l):
+            aminoacid = AlignmentModel.ALPHABET[s[i]]
+            plt.annotate(aminoacid, seq_2d[lsum+i]+anno_offset)
+        lsum += l
+    plt.show()
